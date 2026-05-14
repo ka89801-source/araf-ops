f747a77d34068874872d7bd0ea93efdddce9d6c2
ka89801-source <ka89801@gmail.com>
2026-05-14 15:31:35 +0300

    Update app.js

--- js/app.js
+++ js/app.js
@@ -1005,7 +1005,6 @@ function renderEmployeesPage() {
         </div>
 
         <div class="emp-actions">
-          <button class="btn btn-sm" onclick="showToast('سيتم فتح نموذج تعديل الموظف','default')">
             <svg viewBox="0 0 24 24"><path d="M11 4H4a2 2 0 0 0-2 2v14a2 2 0 0 0 2 2h14a2 2 0 0 0 2-2v-7"/><path d="M18.5 2.5a2.121 2.121 0 1 1 3 3L12 15l-4 1 1-4 9.5-9.5z"/></svg>
             تعديل
           </button>
