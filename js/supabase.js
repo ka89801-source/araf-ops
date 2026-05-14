c5e050bb7eb907e4dc07db44c9d5234aa7b107dd
ka89801-source <ka89801@gmail.com>
2026-05-14 11:42:35 +0300

    Create supabase.js

---
+++ js/supabase.js
@@ -0,0 +1,7 @@
+const SUPABASE_URL = 'https://yuoforvbxpwislmdrvwb.supabase.co';
+const SUPABASE_ANON_KEY = 'sb_publishable_tyyiEKXaSDaUKN_HCLkBGg_vZTYMwki';
+
+window.sb = supabase.createClient(
+  SUPABASE_URL,
+  SUPABASE_ANON_KEY
+);
